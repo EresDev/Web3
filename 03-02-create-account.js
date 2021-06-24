9685adac89a50a0dfd1f1db214aa6a8e7cf45592
EresDev <eresdev@hotmail.com>
2021-06-24 21:50:46 +0500

    feat: create account

---
+++ 03-02-create-account.js
@@ -0,0 +1,6 @@
+const Web3 = require('web3');
+
+const rpcServer = 'https://ropsten.infura.io/v3/9bdfab5e846547cab620fc258709d048';
+const web3 = new Web3(rpcServer);
+
+console.log(web3.eth.accounts.create());
